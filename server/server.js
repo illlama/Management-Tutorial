52fd2481243c5858eb1441eae68b8abbe9724dbf
illlama <beatlemaniakih@gmail.com>
2020-07-07 15:14:25 +0900

    Create customer DB tavle & Node.js connect setting

--- server/server.js
+++ server/server.js
@@ -1,39 +1,30 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const { connect } = require('http2');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: 'https://placeimg.com/64/64/1',
-      name: '홍길동',
-      birthday: 19931221,
-      gender: 'male',
-      job: 'developer',
-    },
-    {
-      id: 2,
-      image: 'https://placeimg.com/64/64/2',
-      name: '정하루',
-      birthday: 19981211,
-      gender: 'female',
-      job: 'developer',
-    },
-    {
-      id: 3,
-      image: 'https://placeimg.com/64/64/3',
-      name: '판다킴',
-      birthday: 19921121,
-      gender: 'female',
-      job: 'developer',
-    },
-  ]);
-  res.end();
+  connection.query('SELECT * FROM CUSTOMER;', (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
